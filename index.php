06b2d48b52446d7897cc49cfa920948c406527c9
Sabrina Altahan <sabrinaltahan157@gmail.com>
2026-04-13 21:54:36 +0400

    created header and hero section for homepage

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ARAK Wood</title>
+  
+ <!------css style------>
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+  <!-- Header -->
+  <header class="site-header">
+    <div class="container header-container">
+
+      <!-- Logo -->
+      <a href="index.php" class="logo">
+        <img src="assets/images/logo.png" alt="ARAK Wood Logo">
+      </a>
+
+      <!-- Navigation -->
+      <nav class="navbar" id="navbar">
+        <ul class="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#projects">Products</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#why-arak">About Us</a></li>
+          <li><a href="#footer">Contact</a></li>
+        </ul>
+      </nav>
+
+      <!-- CTA -->
+      <a href="#" class="header-btn">Request a Quote</a>
+
+      <!-- Mobile Menu -->
+      <button class="menu-toggle" id="menuToggle">&#9776;</button>
+    </div>
+  </header>
+
+   <!-- Hero Section -->
+  <section class="hero" id="home">
+    <video autoplay muted loop playsinline class="hero-video">
+      <source src="assets/videos/hero-video.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+
+    <div class="hero-overlay"></div>
+
+    <div class="hero-content">
+      <h1>Crafting Quality Woodwork</h1>
+      <p>
+        Custom furniture, CNC cutting, and interior wood solutions
+        designed with quality and precision.
+      </p>
+      <a href="request-quote.php" class="btn">Request a Quote</a>
+    </div>
+  </section>
+
+
+
+  <!------java script------>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
